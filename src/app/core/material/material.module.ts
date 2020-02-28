427ea758699fa9a9721aac282065fd5644e17d11
Konrad Molik <kmolik@zuw.szczecin.pl>
2020-02-28 15:46:35 +0100

    test

--- src/app/core/material/material.module.ts
+++ src/app/core/material/material.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import {MatTableModule} from '@angular/material/table';
 import { MatButtonModule} from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
 
 
 
 const material = [
   MatTableModule,
-  MatButtonModule
+  MatButtonModule,
+  MatCardModule
 ];
 
 
