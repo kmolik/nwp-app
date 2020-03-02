253fee9d9537f0c099a4f56717a252820e08ca83
Konrad Molik <kmolik@zuw.szczecin.pl>
2020-03-02 12:31:51 +0100

    table rework

--- src/app/modules/economy/unit-index/unit-index-routing.module.ts
+++ src/app/modules/economy/unit-index/unit-index-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { TableComponent } from './table/table.component';
+import { TableComponent } from './unitsTable/unitsTable.component';
 
 
 const routes: Routes = [
--- src/app/modules/economy/unit-index/unitsTable/unitsTable.component.ts
+++ src/app/modules/economy/unit-index/unitsTable/unitsTable.component.ts
@@ -1,22 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { UnitIndexService } from 'src/app/core/http/unit_index.service';
 
+
 @Component({
   selector: 'app-table',
-  templateUrl: './table.component.html',
-  styleUrls: ['./table.component.sass']
+  templateUrl: './unitsTable.component.html',
+  styleUrls: ['./unitsTable.component.sass']
 })
 export class TableComponent implements OnInit {
 
-  displayedColumns: string[] = ['id', 'nazwa_jednostki', 'adres_jednostki'];
-
-  public units = [];
+  displayedColumns: string[] = ['id', 'unit_name', 'unitAdres'];
+  dataSource = [];
 
   constructor(private unitIndexService: UnitIndexService) { }
 
   ngOnInit() {
     this.unitIndexService.getUnits()
-      .subscribe(data => this.units = data);
+      .subscribe(data => this.dataSource = data);
   }
 
 }
