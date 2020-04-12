8dc81c2bbc97dc035d4679b34ea95785c3b3da72
kmolik <48009091+kmolik@users.noreply.github.com>
2020-04-12 15:51:44 +0200

    veryfi token for db

--- api/routes/buildings.routes.js
+++ api/routes/buildings.routes.js
@@ -1,8 +1,18 @@
-const buildings = require('../db/models/buildings')
 const controller = require('../controllers/buildings.controller')
-
+const { authJwt } = require('../middlewares')
 
 module.exports = function(app) {
-  app.get('/buildings', controller.getBuildings)
+  app.use(function(req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "authorization, Origin, Content-Type, Accept"
+    )
+    next()
+  })
+
+  app.get('/buildings',
+  [authJwt.verifyToken],
+  controller.getBuildings
+   )
   }
 
