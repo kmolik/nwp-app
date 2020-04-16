8130b47826c654ebf25dd22f527a9838097b5360
kmolik <48009091+kmolik@users.noreply.github.com>
2020-04-16 15:17:14 +0200

    minor fixes

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { StorageService } from '../services';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private storageService: StorageService
+  ) { }
 
   ngOnInit() {
   }
 
+  logout() {
+    this.router.navigate(['/login']);
+    this.storageService.deleteLocalStorageItem('token');
+    console.log('Loged Out');
+  }
 }
