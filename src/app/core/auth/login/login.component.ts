731f912638180b06bd640f688db6f425b19aa0e3
kmolik <48009091+kmolik@users.noreply.github.com>
2020-05-04 15:41:42 +0200

    responsive login page

--- src/app/core/auth/login/login.component.ts
+++ src/app/core/auth/login/login.component.ts
@@ -8,7 +8,7 @@ import { Token } from '../../models/auth';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.sass']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
